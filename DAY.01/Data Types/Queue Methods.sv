c443eaad1294abf9cd055d38d959f39a213e03ee
ADHIKESAVAN . M <adhik8108@gmail.com>
2025-11-07 12:34:11 +0530

    Update Queue Methods.sv

--- DAY.01/Data Types/Queue Methods.sv
+++ DAY.01/Data Types/Queue Methods.sv
@@ -26,4 +26,35 @@
       q.shuffle();
       $display(q);
     end
+ endmodule
+
+// Queue Method Using Bounded Type
+module queues;
+  int j;
+  int q[$:4] = '{0,2,5,4,6,7};
+  initial 
+    begin
+      j = 1;
+      q.insert(1,j);
+      $display("after insert = ",q);
+      q.delete(1);
+      $display("after deleting =",q);
+      q.push_front(7);
+      $display("after push_front =",q);
+      q.push_back(9);
+      $display("after push_back =",q);
+      j = q.pop_back();
+      $display(j,",",q);
+      j = q.pop_front;
+      $display(j,",",q);
+      q.reverse();
+      $display("after reversing =",q);
+      q.sort();
+      $display("after sorting =",q);
+      q.rsort();
+      $display("reverse sorting =",q);
+      q.shuffle();
+      $display("after shuffle =",q);
+    end
 endmodule
+      
