bda9227de47fa33739e1d97cd2caeee8f052bace
ADHIKESAVAN . M <adhik8108@gmail.com>
2025-11-20 09:18:32 +0530

    Update For_loop.sv

--- DAY.07/Loops in SV/For_loop.sv
+++ DAY.07/Loops in SV/For_loop.sv
@@ -10,3 +10,15 @@ module for_loop;
       $display("a[%0d] = [%0d]",i,a[i]);
   end
 endmodule
+
+/*Contains Synopsys proprietary information.
+Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;  Nov 19 22:45 2025
+a[0] = [0]
+a[1] = [2]
+a[2] = [4]
+a[3] = [6]
+a[4] = [8]
+           V C S   S i m u l a t i o n   R e p o r t 
+Time: 0 ns
+CPU Time:      0.400 seconds;       Data structure size:   0.0Mb
+Wed Nov 19 22:45:58 2025
