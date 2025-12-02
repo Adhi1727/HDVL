01d5d7ea775a791f3a5ae39f8287b2675cae2d6a
ADHIKESAVAN . M <adhik8108@gmail.com>
2025-12-02 21:35:16 +0530

    Create 08. Associative arr datatype.sv

---
+++ Assignments/SV Assignment1/08. Associative arr datatype.sv
@@ -0,0 +1,9 @@
+/*8. Write a test bench to test the Associative array data type and its predefined
+methods by using the following statements
+a. declare a 64-bit integer type sparse array assoc & an index idx
+b. initialize idx to 1
+c. fill array assoc by 1-bit left shift of idx value in a loop
+which runs 64 times so that the array is filled as a
+sparse array
+d. use first, next, last, prev methods to get the values of
+these elements*/
