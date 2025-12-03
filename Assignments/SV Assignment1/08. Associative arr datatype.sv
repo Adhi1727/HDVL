0ec84dd6e23d17f9c5fd338f7f09f9f99037c4a0
ADHIKESAVAN . M <adhik8108@gmail.com>
2025-12-03 10:43:17 +0530

    Update 08. Associative arr datatype.sv

--- Assignments/SV Assignment1/08. Associative arr datatype.sv
+++ Assignments/SV Assignment1/08. Associative arr datatype.sv
@@ -7,3 +7,38 @@ which runs 64 times so that the array is filled as a
 sparse array
 d. use first, next, last, prev methods to get the values of
 these elements*/
+module sparse;
+  int unsigned assoc[int];
+  int idx;
+  initial begin
+    idx = 1;
+    repeat(64) begin
+      assoc[idx] = idx;
+      idx = idx<<1;
+    end
+    
+    $display("First value");
+    if(assoc.first(idx))
+      $display("idx first value =  ",assoc[idx]);
+    $display("next value");
+    if(assoc.next(idx))
+      $display("idx next value = ",assoc[idx]);
+    $display("last value");
+    if(assoc.last(idx))
+      $display("idx last value = ",assoc[idx]);
+    $display("previous value");
+    if(assoc.prev(idx))
+      $display("idx pervious value = ",assoc[idx]);
+  end
+endmodule
+/*
+# KERNEL: First value
+# KERNEL: idx first value =  2147483648
+# KERNEL: next value
+# KERNEL: idx next value =          0
+# KERNEL: last value
+# KERNEL: idx last value = 1073741824
+# KERNEL: previous value
+# KERNEL: idx pervious value =  536870912
+# KERNEL: Simulation has finished. There are no more test vectors to simulate.
+# VSIM: Simulation has finished.
