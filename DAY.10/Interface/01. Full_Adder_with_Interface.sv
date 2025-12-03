a0afe4c0de98d73a9e7f2734ed359454eb1e7aac
ADHIKESAVAN . M <adhik8108@gmail.com>
2025-12-03 21:11:13 +0530

    Update 01. Full_Adder_with_Interface.sv

--- DAY.10/Interface/01. Full_Adder_with_Interface.sv
+++ DAY.10/Interface/01. Full_Adder_with_Interface.sv
@@ -1,16 +1,15 @@
 // Design code
-module tb_top;           
-  fa_if inf();
-  full_adder fa(inf);
+module half_addr(input a, b, output so, co);
+  assign so = a ^ b;
+  assign co = a & b;
+endmodule
+
+module full_adder(fa_if inf);
+  wire s0, c0, c1;
+  half_addr ha1 (inf.a, inf.b, s0, c0);
+  half_addr ha2 (s0, inf.c, inf.s_out, c1);
   
-  initial begin
-    $monitor("a=%b b=%b c=%b,sum=%b,carry=%b",inf.a,inf.b,inf.c,inf.s_out,inf.c_out);
-    inf.a = 1; inf.b = 0; inf.c = 0;
-    #1;
-    inf.a = 1; inf.b = 1; inf.c = 1;
-    #1;
-    inf.a = 0; inf.b = 1; inf.c = 1;
-  end
+  assign inf.c_out = c0 | c1;
 endmodule
 
 // Interface
