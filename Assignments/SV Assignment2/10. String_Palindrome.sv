4014ba90dc101a72bf5568c506361821e2266acd
ADHIKESAVAN . M <adhik8108@gmail.com>
2025-12-07 08:04:09 +0530

    Update 10. String_Palindrome.sv

--- Assignments/SV Assignment2/10. String_Palindrome.sv
+++ Assignments/SV Assignment2/10. String_Palindrome.sv
@@ -1,1 +1,23 @@
-//10
+// 10. Write a module that checks if a string is a palindrome (reads the same forwards and backwards). For example, check if "racecar" is a palindrome.
+module exmaple;
+  string str;
+  string s;
+  initial begin
+    str = "level";
+    $display("string str = %s",str);
+    
+    for (int i = str.len() - 1; i >= 0; i--) 
+      s = {s, str[i]};
+    $display("reverse string = %s", s);
+    if (str == s)
+      $display("String is an Palindrome");
+    else 
+      $display("String is not an palindrome");
+  end
+endmodule
+/*
+# KERNEL: string str = level
+# KERNEL: reverse string = level
+# KERNEL: String is an Palindrome
+# KERNEL: Simulation has finished. There are no more test vectors to simulate.
+# VSIM: Simulation has finished.
