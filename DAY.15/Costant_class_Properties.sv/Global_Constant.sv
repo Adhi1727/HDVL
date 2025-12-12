f8d51d5dad90d8958bfbaa2ac6a1302b0f9b5ed4
ADHIKESAVAN . M <adhik8108@gmail.com>
2025-12-12 20:13:15 +0530

    Update Global_Constant.sv

--- DAY.15/Costant_class_Properties.sv/Global_Constant.sv
+++ DAY.15/Costant_class_Properties.sv/Global_Constant.sv
@@ -1,1 +1,23 @@
-//
+// class properties can be made read-only by a "const" declaration.... Since class objects are dynamic in nature!...
+// Global constant => An initial value is assigned during variable declaration . Global constants are also declared static...
+class packet;
+  bit [3:0] addr;
+  const int data = 8; // Global constant declared
+  
+  function void display();
+    $display("addr = %0d, data = %0d",addr,data);
+  endfunction
+endclass
+
+module example;
+  packet p1;
+  initial begin
+    p1 = new();
+    p1.addr = 4'h2;
+    p1.display();
+  end
+endmodule
+/*
+# KERNEL: addr = 2, data = 8
+# KERNEL: Simulation has finished. There are no more test vectors to simulate.
+# VSIM: Simulation has finished.
