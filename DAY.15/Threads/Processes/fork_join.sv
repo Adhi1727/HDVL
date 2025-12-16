15b58fcb0f54a5e58e8570f05e7136a4dc707ea0
ADHIKESAVAN . M <adhik8108@gmail.com>
2025-12-16 18:34:20 +0530

    Update fork_join.sv

--- DAY.15/Threads/Processes/fork_join.sv
+++ DAY.15/Threads/Processes/fork_join.sv
@@ -1,1 +1,2 @@
-//
+// Thread is a lightweight process created inside another process... sv allows multiple threads inside a single process and threads run concurrently!..
+// fork... join => Start all threads in parallel and wait until every thread finishes and statement are sequentially one after another
