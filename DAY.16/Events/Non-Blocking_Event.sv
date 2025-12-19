71c4bbf3cb716aed8cfa00ba8d4281a7c7a7bb3f
ADHIKESAVAN . M <adhik8108@gmail.com>
2025-12-19 11:05:24 +0530

    Update Non-Blocking_Event.sv

--- DAY.16/Events/Non-Blocking_Event.sv
+++ DAY.16/Events/Non-Blocking_Event.sv
@@ -1,1 +1,28 @@
-//
+module example;
+  event start;
+  
+  initial begin
+    $display("%0t\t Before the event start triggering",$time);
+    ->>start;
+    $display("%0t\t After the event start triggered",$time);
+  end
+  
+  initial begin
+    $display("%0t\t Waiting for event start is triggered",$time);
+    @start;
+    $display("%0t\t Event start is triggered",$time);
+  end
+  
+  initial begin
+    $display("%0t\t waiting for event start to triggered using wait opr",$time);
+    wait(start.triggered);
+    $display("%0t\t Event start is triggered using wait opr",$time);
+  end
+endmodule
+/*
+# KERNEL: 0	 Before the event start triggering
+# KERNEL: 0	 After the event start triggered
+# KERNEL: 0	 Waiting for event start is triggered
+# KERNEL: 0	 waiting for event start to triggered using wait opr
+# KERNEL: 0	 Event start is triggered
+# KERNEL: 0	 Event start is triggered using wait opr
